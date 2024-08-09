9f4906bd296b0b1e2b27511e72b75f74266c5f9a
kaviyasree26 <150135134+kaviyasree26@users.noreply.github.com>
2024-08-09 16:10:13 +0530

    Add files via upload

---
+++ JobSearchingAppbackend/job/src/main/java/com/example/job/model/Company.java
@@ -0,0 +1,25 @@
+package com.example.job.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Company {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY )
+    private int companyId;
+    private String companyName;
+    private String emailId;
+    private String password;
+
+}
---
+++ JobSearchingAppbackend/job/src/main/java/com/example/job/repository/Adminrepo.java
@@ -0,0 +1,13 @@
+package com.example.job.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.job.model.Admin;
+// import java.util.List;
+
+
+public interface Adminrepo extends JpaRepository<Admin,Integer>
+ {
+  
+
+} 
